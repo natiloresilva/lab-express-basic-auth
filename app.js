f5b70062831bf5c4a2c9d7c20af71d0e1bd6259a
Natalia Silva <natalia.pupushi@gmail.com>
2020-07-27 20:20:18 -0300

    basic-auth-ready

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
@@ -16,6 +19,17 @@ const app = express();
 
 // require database configuration
 require('./configs/db.config');
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  resave: true,
+  saveUninitialized: true
+}));
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -32,7 +46,10 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-const index = require('./routes/index.routes');
-app.use('/', index);
+const homeRouter = require('./routes/index.routes');
+const authRouter = require("./routes/auth");
+
+app.use("/", authRouter);
+app.use('/', homeRouter);
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('home'));
+router.get('/index', (req, res) => res.render('index'));
+
+
+// verificamos si el usuario tiene una session activa, de ser así, lo redirigimos a la siguiente ruta, en este caso
+// /secret
+// en caso contrario, redirigimos al usuario a /login
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) { // <== if there's user in the session (user is logged in)
+    next(); // ==> go to the next route 
+  } else {
+    res.redirect("/login");
+  }
+});
+
+// renderizamos la plantilla secret.hbs con el username
+// deconstruimos en la variable username el username de req.session.currentUser
+
+router.get('/main', (req, res) => res.render('main'));
+router.get('/private', (req, res) => res.render('private'));
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    // si no puede acceder a los datos de sesión, redirige a /login
+    res.redirect("/login");
+  });
+});
+
 
 module.exports = router;
